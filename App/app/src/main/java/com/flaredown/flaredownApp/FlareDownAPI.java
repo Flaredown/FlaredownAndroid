6629cccbf6ba433c240639b31bdd5ee167782edb
Thomas Hunter <th@thomashunter.co.uk>
2015-09-03 12:49:06 +0100

    Forgot to stage files of previous commit

--- App/app/src/main/java/com/flaredown/flaredownApp/FlareDownAPI.java
+++ App/app/src/main/java/com/flaredown/flaredownApp/FlareDownAPI.java
@@ -4,4 +4,7 @@ package com.flaredown.flaredownApp;
  * Created by thunter on 03/09/15.
  */
 public class FlareDownAPI {
+    public static final String API_DOMAIN = "https://api-staging.flaredown.com/v1";
+
+    public void sign_in
 }
--- App/app/src/main/java/com/flaredown/flaredownApp/FlareDownLocales.java
+++ App/app/src/main/java/com/flaredown/flaredownApp/FlareDownLocales.java
@@ -4,4 +4,7 @@ package com.flaredown.flaredownApp;
  * Created by thunter on 03/09/15.
  */
 public class FlareDownLocales {
+    public void updateLocales() {
+        
+    }
 }
