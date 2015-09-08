69ea74a8d596159bb4f2cc691c6dd3703d48c91a
Thomas Hunter <th@thomashunter.co.uk>
2015-09-08 14:45:24 +0100

    Seperate home activity layout for wider displays (landscape phones & tablets) preventing the navigation draw extending too much.

--- App/app/src/main/java/com/flaredown/flaredownApp/HomeActivity.java
+++ App/app/src/main/java/com/flaredown/flaredownApp/HomeActivity.java
@@ -9,8 +9,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
