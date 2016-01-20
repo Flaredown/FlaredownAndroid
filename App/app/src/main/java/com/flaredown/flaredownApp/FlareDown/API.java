8448527460f4ffdfe24796f94974adfc71a41244
Thomas Hunter <th@thomashunter.co.uk>
2016-01-20 09:56:28 +0000

    Changed api uri

--- App/app/src/main/java/com/flaredown/flaredownApp/FlareDown/API.java
+++ App/app/src/main/java/com/flaredown/flaredownApp/FlareDown/API.java
@@ -40,7 +40,7 @@ public class API {
     private static final String SP_USER_EMAIL = "FlareDownAPI_useremail"; // String
     private static final String SP_USER_SIGNED_IN = "FlareDownAPI_signedin"; // Boolean
     private static final String SP_ENTRIES_CACHE = "FlareDownAPI_entries_cache";
-    public static final String API_BASE_URL = "https://api-staging.flaredown.com/v1";
+    public static final String API_BASE_URL = "https://staging.flaredown.com/v1";
     public static final SimpleDateFormat API_DATE_FORMAT= new SimpleDateFormat("MMM-dd-yyyy");
     private static final String LOCALE_CACHE_FNAME = "localeCache";
     public static final String CHAR_SET = "UTF-8";
@@ -347,7 +347,6 @@ public class API {
     public void submitEntry(final Date date, final JSONObject response, final OnApiResponse<JSONObject> onApiResponse) {
         HashMap<String, String> params = new HashMap<>();
         params.put("entry", response.toString());
-
         final Runnable retryRunnable = new Runnable() {
             @Override
             public void run() {
