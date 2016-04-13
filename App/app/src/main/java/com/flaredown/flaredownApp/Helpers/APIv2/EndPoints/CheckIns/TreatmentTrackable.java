d399a5039db301b1dc7a2c18172e751681115706
Thomas Hunter <th@thomashunter.co.uk>
2016-04-14 00:12:11 +0100

    [BUG FIX] Fixed hasResponse returning true
    hasResponse would return true when a user is tracking a treatment. This
    is due to the treatment by default having a value.

---
+++ App/app/src/main/java/com/flaredown/flaredownApp/Helpers/APIv2/EndPoints/CheckIns/TreatmentTrackable.java
@@ -0,0 +1,56 @@
+package com.flaredown.flaredownApp.Helpers.APIv2.EndPoints.CheckIns;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Used as a base to represent a treatment trackable
+ */
+public class TreatmentTrackable extends Trackable {
+
+    private Boolean isTaken = false;
+
+    /**
+     * Default constructor for the treatment trackable object.
+     */
+    public TreatmentTrackable() {
+        super(TrackableType.TREATMENT);
+    }
+
+    /**
+     * Create a treatment object from a JSON representing a treatment object.
+     * @param jsonObject Representing a treatment object.
+     */
+    public TreatmentTrackable(JSONObject jsonObject) {
+        super(TrackableType.TREATMENT, jsonObject);
+        this.isTaken = jsonObject.optBoolean("is_taken", false);
+    }
+
+    public Boolean getIsTaken() {
+        return isTaken;
+    }
+
+    public void setIsTaken(Boolean taken) {
+        isTaken = taken;
+    }
+
+    @Override
+    public JSONObject toJson() throws JSONException {
+        JSONObject rootJObject = super.toJson();
+        rootJObject.put("is_taken", isTaken);
+        return rootJObject;
+    }
+
+    /**
+     * Get the response json for a single trackable.
+     * @return The response json for a single trackable.
+     * @throws JSONException
+     */
+    @Override
+    public JSONObject getResponseJson() throws JSONException {
+        JSONObject rootJObject = super.getResponseJson();
+        rootJObject.put("is_taken", isTaken);
+        return rootJObject;
+    }
+}
