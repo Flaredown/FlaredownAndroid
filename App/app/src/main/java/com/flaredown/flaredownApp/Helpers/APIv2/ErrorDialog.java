5c38b99f99fd96f093dd4598f7bc46c7a4c19312
Thomas Hunter <th@thomashunter.co.uk>
2016-05-21 21:37:17 +0100

    Error Dialog provides better messages.
    Error dialog provides more detailed messages… differentiating from the
    phone having no internet connectivity and no response from server. Also
    displaying different messages depending on the status code of the error.
    
    + Fixed IME keyboard issue on login... When editing password the next button does not submit the check in.
    + Fixed error messages (replacing {{field}}) with the field name.

--- App/app/src/main/java/com/flaredown/flaredownApp/Helpers/APIv2/ErrorDialog.java
+++ App/app/src/main/java/com/flaredown/flaredownApp/Helpers/APIv2/ErrorDialog.java
@@ -4,73 +4,110 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.support.annotation.NonNull;
 import android.text.Html;
 
+import com.flaredown.flaredownApp.Helpers.InternetConnectivity;
 import com.flaredown.flaredownApp.Helpers.PreferenceKeys;
-import com.flaredown.flaredownApp.Helpers.Styling.Styling;
+import com.flaredown.flaredownApp.Helpers.StringHelper;
 import com.flaredown.flaredownApp.R;
+import com.flaredown.flaredownApp.Receivers.InternetStatusBroadcastReceiver;
+
+import java.util.List;
 
 /**
  * Displays a error dialog to the user. Created from the Error object past at construction.
  */
 public class ErrorDialog {
     private static final String DEBUG_KEY = "ErrorDialog";
-    Error apiError;
-    Context context;
-    boolean cancelable;
+    private Error apiError;
+    private Context context;
+    private boolean cancelable = true;
+    private InternetStatusBroadcastReceiver internetStatusBroadcastReceiver;
 
-    public ErrorDialog(Context context, Error apiError, boolean cancelable) {
+    public ErrorDialog(@NonNull Context context, @NonNull Error apiError) {
         this.apiError = apiError;
         this.context = context;
-        this.cancelable = cancelable;
-        constructDialog();
     }
 
+    /**
+     * Should the user be able to cancel the error dialog.
+     * @param cancelable True if the user can cancel the error dialog.
+     * @return Itself.
+     */
+    public ErrorDialog setCancelable(boolean cancelable) {
+        this.cancelable = cancelable;
+        return this;
+    }
 
-    private void constructDialog() {
-        // TODO Display errors with the correct error message.
-        // TODO Give option the restrict closing the dialog.
+    /**
+     * Show the error dialog.
+     */
+    public void show() {
         String title = "", description = "";
+        final boolean internetConnected = InternetConnectivity.isConnected(context);
 
-        if(context == null) {
-            PreferenceKeys.log(PreferenceKeys.LOG_E, DEBUG_KEY, getExtraDebugginString());
+        // Set the title and description of the dialog.
+        title = context.getString(R.string.locales_nice_errors_general_error); // Default value.
+        description = context.getString(R.string.locales_nice_errors_general_error_description); // Default values.
+
+        if(!internetConnected) {
+            description = context.getString(R.string.locales_nice_errors_no_internet);
+        } else if(!apiError.isResponseGiven()) {
+            description = context.getString(R.string.locales_nice_errors_no_response);
         } else {
-            title = context.getString(R.string.locales_nice_errors_general_error);
-            description = context.getString(R.string.locales_nice_errors_general_error_description);
-
-            if(PreferenceKeys.DEBUGGING) {
-                description += getExtraDebugginString();
-            }
-
-            // Display the pop up.
-            AlertDialog.Builder builder = new AlertDialog.Builder(context);
-
-            builder.setTitle(title);
-            builder.setMessage(Html.fromHtml(description));
-            builder.setCancelable(cancelable);
-
-            if(cancelable) {
-                builder.setPositiveButton(context.getString(R.string.locales_nav_ok), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
+            String identifier = "locales_nice_errors_" + apiError.getStatusCode();
+            int resId = context.getResources().getIdentifier(identifier, "string", context.getPackageName());
+            if(resId != 0) { // Only if resId found.
+                description = context.getString(resId);
+            } else {
+                List<String> errorMessages = apiError.getErrorList();
+                boolean first = true;
+                if(errorMessages.size() > 0) {
+                    description = "";
+                    for (String errorMessage : errorMessages) {
+                        if(!first) description += "<br/>";
+                        description += StringHelper.forceFullStop(StringHelper.upperFirstChar(errorMessage));
+                        first = false;
                     }
-                });
+                }
             }
-
-            if(apiError.getRetryRunnable() != null) {
-                builder.setNegativeButton(context.getString(R.string.locales_nav_retry), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        apiError.getRetryRunnable().run();
-                    }
-                });
-            }
-
-            Dialog dialog = builder.create();
-//            Styling.styleDialog(dialog);
-            dialog.show();
         }
+
+        if(PreferenceKeys.DEBUGGING) { // Append extra information if debugging
+            description += getExtraDebugginString();
+        }
+
+        // Build the dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        builder.setTitle(title);
+        builder.setMessage(Html.fromHtml(description));
+
+        // Set the buttons.
+        // Ok button.
+        if(cancelable) {
+            builder.setPositiveButton(context.getString(R.string.locales_nav_ok), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // When clicked the dialog will automatically close.
+                }
+            });
+        }
+        builder.setCancelable(cancelable); // If not cancelable prevent user from closing dialog.
+
+        // Retry button.
+        if(apiError.getRetryRunnable() != null) {
+            builder.setNegativeButton(context.getString(R.string.locales_nav_retry), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    apiError.getRetryRunnable().run();
+                }
+            });
+        }
+
+        final Dialog dialog = builder.create();
+        dialog.show();
     }
 
     /**
--- App/app/src/main/java/com/flaredown/flaredownApp/Login/LoginActivity.java
+++ App/app/src/main/java/com/flaredown/flaredownApp/Login/LoginActivity.java
@@ -85,7 +85,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.et_login || id == EditorInfo.IME_NULL) {
+                if (id == EditorInfo.IME_ACTION_GO) {
                     attemptLogin();
                     return true;
                 }
@@ -169,18 +169,18 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 
         // Check for a valid password, if the user entered one.
         if (TextUtils.isEmpty(password) || !isPasswordValid(password)) {
-            mPasswordView.setError(getResources().getString(R.string.locales_nice_errors_field_invalid));
+            mPasswordView.setError(getResources().getString(R.string.locales_nice_errors_field_invalid).replace("{{field}}", "Password"));
             focusView = mPasswordView;
             cancel = true;
         }
 
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getResources().getString(R.string.locales_nice_errors_field_required));
+            mEmailView.setError(getResources().getString(R.string.locales_nice_errors_field_required).replace("{{field}}", "Email"));
             focusView = mEmailView;
             cancel = true;
         } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.locales_nice_errors_field_invalid));
+            mEmailView.setError(getString(R.string.locales_nice_errors_field_invalid).replace("{{field}}", "Email"));
             focusView = mEmailView;
             cancel = true;
         }
@@ -224,7 +224,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
                         mEmailView.setError(errorMessage);
                         mPasswordView.setError(errorMessage);
                     } else
-                        new ErrorDialog(mContext, result, false);
+                        new ErrorDialog(mContext, result).setCancelable(true).show();
                 }
             });
         }
