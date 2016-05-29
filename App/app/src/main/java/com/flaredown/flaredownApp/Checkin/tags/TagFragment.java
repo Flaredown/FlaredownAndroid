88b8fa4e272c61384b4d1b10e318425ad45ddee2
Thomas Hunter <th@thomashunter.co.uk>
2016-05-29 16:06:40 +0100

    Tag Styling
    Styling of the tag view for the tag card in the check in

--- App/app/src/main/java/com/flaredown/flaredownApp/Checkin/tags/TagFragment.java
+++ App/app/src/main/java/com/flaredown/flaredownApp/Checkin/tags/TagFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -15,6 +16,7 @@ import com.flaredown.flaredownApp.Helpers.APIv2.APIResponse;
 import com.flaredown.flaredownApp.Helpers.APIv2.Communicate;
 import com.flaredown.flaredownApp.Helpers.APIv2.EndPoints.Tag;
 import com.flaredown.flaredownApp.Helpers.APIv2.Error;
+import com.flaredown.flaredownApp.Helpers.FlowLayoutHelper;
 import com.flaredown.flaredownApp.R;
 
 import org.apmem.tools.layouts.FlowLayout;
@@ -30,6 +32,10 @@ public class TagFragment extends ViewPagerFragmentBase {
 
     private FrameLayout fl_root;
     private FlowLayout fl_popular_tags;
+    private FlowLayoutHelper<Tag> flh_popular_tags;
+
+    private FlowLayout fl_selected_tags;
+    private FlowLayoutHelper<Tag> flh_selected_tags;
 
     /**
      * Create a new instance for the fragment.
@@ -57,21 +63,23 @@ public class TagFragment extends ViewPagerFragmentBase {
      * Get and display the list of popular tags.
      */
     private void displayPopularTags() {
-        fl_popular_tags.removeAllViews();
+        flh_popular_tags.clear();
         fl_popular_tags.addView(new ProgressBar(getActivity()));
         api.getPopularTags(new APIResponse<List<Tag>, Error>() {
             @Override
             public void onSuccess(List<Tag> result) {
-                fl_popular_tags.removeAllViews();
+                flh_popular_tags.clear();
                 for (Tag tag : result) {
-                    TextView tv = addPopularTag(tag.getName());
+                    flh_popular_tags.addItem(tag);
                 }
             }
 
             @Override
             public void onFailure(Error result) {
                 fl_popular_tags.removeAllViews();
-                TextView tv = addPopularTag("Error Loading Retry?");
+                TextView tv = new TextView(getActivity());
+                tv.setText("Error Loading Retry?");
+                fl_popular_tags.addView(tv);
                 tv.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -82,14 +90,6 @@ public class TagFragment extends ViewPagerFragmentBase {
         });
     }
 
-    private TextView addPopularTag(String tag) {
-        TextView tv = new TextView(getActivity());
-        tv.setTextAppearance(getActivity(), R.style.AppTheme_TextView_Link);
-        tv.setText(tag);
-        fl_popular_tags.addView(tv);
-        return tv;
-    }
-
     /**
      * Inflates and assigns views.
      * @param inflater
@@ -98,5 +98,40 @@ public class TagFragment extends ViewPagerFragmentBase {
     private void assignViews(LayoutInflater inflater, ViewGroup container) {
         fl_root = (FrameLayout) inflater.inflate(R.layout.checkin_fragment_tags, container, false);
         fl_popular_tags = (FlowLayout) fl_root.findViewById(R.id.fl_popular_tags);
+        flh_popular_tags = new FlowLayoutHelper<>(fl_popular_tags, new FlowLayoutHelper.Adapter<Tag>() {
+            @Override
+            public View viewCreation(final Tag item) {
+                TextView tv = new TextView(getActivity());
+                tv.setText(item.getName());
+                tv.setTextAppearance(getActivity(), R.style.AppTheme_TextView_Link);
+                tv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        flh_selected_tags.addItem(item);
+                    }
+                });
+                int padding = (int) getResources().getDimension(R.dimen.sep_margin_small);
+                tv.setPadding(padding, padding, padding, padding);
+                return tv;
+            }
+        });
+
+        fl_selected_tags = (FlowLayout) fl_root.findViewById(R.id.fl_selected_tags);
+        flh_selected_tags = new FlowLayoutHelper<>(fl_selected_tags, new FlowLayoutHelper.Adapter<Tag>() {
+            @Override
+            public View viewCreation(final Tag item) {
+                LinearLayout linearLayout = (LinearLayout) getActivity().getLayoutInflater().inflate(R.layout.checkin_template_tag, null, false);
+                TextView tv = (TextView) linearLayout.findViewById(R.id.tv_tag);
+                tv.setText(item.getName());
+//                ((ViewGroup.MarginLayoutParams) tv.getLayoutParams()).setMargins(100, 100, 100, 100);
+                tv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        flh_selected_tags.removeItem(item);
+                    }
+                });
+                return linearLayout;
+            }
+        });
     }
 }
