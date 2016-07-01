4a69c538c7e9cd5d343cb9a897a95ddc7498697c
squiggie <squigge@gmail.com>
2016-07-01 14:15:57 -0500

    [Issue # 43] - Change Intercom API Key to use api.properties file Update Realm Update .gitignore to exclude api.properties [Issue #41] - Handle RealmMigrationNeeded Exception with Migration object and schema versions [Issue #40] - Remove Alarms when user logs out

--- App/app/src/main/java/com/flaredown/flaredownApp/FlaredownApp.java
+++ App/app/src/main/java/com/flaredown/flaredownApp/FlaredownApp.java
@@ -3,16 +3,26 @@ package com.flaredown.flaredownApp;
 import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
+import com.flaredown.flaredownApp.Helpers.Migration;
 
-import io.fabric.sdk.android.*;
+import io.fabric.sdk.android.Fabric;
 import io.intercom.android.sdk.Intercom;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 
-public class FlaredownApp extends Application {
+public class FlaredownApp extends Application{
 
     @Override
     public void onCreate() {
         super.onCreate();
         Fabric.with(this, new Crashlytics());
-        Intercom.initialize(this, BuildConfig.INTERCOM_API, BuildConfig.INTERCOM_SECRET);
+        Intercom.initialize(this, BuildConfig.INTERCOM_API, BuildConfig.APPLICATION_ID);
+
+        //Default Realm Configuration
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder(this)
+                .schemaVersion(1)
+                .migration(new Migration())
+                .build();
+        Realm.setDefaultConfiguration(realmConfiguration);
     }
 }
